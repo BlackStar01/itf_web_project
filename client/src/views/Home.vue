e4a2a6b60c2dbff4356e2b50e372794c93907250
“BlackStar01” <vitalwarren@yahoo.com>
2022-10-17 18:09:00 +0200

    Fix bug and delete residual files

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,15 @@
 <template>
-    <div>
-        <div v-if="action === 'login'">
-            <Register></Register>
-        </div>
-        <div v-else>
-            <Login></Login>
-        </div>
+    <div id="home">
+        <nav>
+
+        </nav>
+        <main>
+            
+        </main>
     </div>
 </template>
 
 <script setup>
-import Register from '../components/Authentification/Register.vue'
-import Login from '../components/Authentification/Login.vue'
 import { ref, onMounted } from 'vue';
 
 const action = ref('login')
